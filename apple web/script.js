e8b03e9831d8db38380cb11f5379fe76b9a17311
ChenJingWei9 <101557543+ChenJingWei9@users.noreply.github.com>
2023-10-07 11:06:04 +0800

    Add files via upload

---
+++ apple web/script.js
@@ -0,0 +1,20 @@
+// 在HTML元素上添加事件监听器
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('按钮被点击了！');
+});
+
+// 改变元素内容
+document.getElementById('myElement').innerHTML = '新的内容';
+
+// 控制流程
+if (condition) {
+    // 条件成立时执行的代码
+} else {
+    // 条件不成立时执行的代码
+}
+
+// 定义函数
+function myFunction(param1, param2) {
+    // 函数体
+}
+    
